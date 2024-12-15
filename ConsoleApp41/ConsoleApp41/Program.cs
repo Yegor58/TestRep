d27a0ee047f3ea9b9947b98a2beecb53e2165405
Yegor58 <egor.vasy@mail.ru>
2024-12-15 11:14:33 +0300

    Commit 2

--- ConsoleApp41/ConsoleApp41/Program.cs
+++ ConsoleApp41/ConsoleApp41/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp41
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("This is a C# console application.");
         }
     }
 }
